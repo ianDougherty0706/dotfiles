dee44c9282c863dec932c179ed30071d088aef98
ianDougherty0706 <idougherty0706@gmail.com>
2025-04-15 21:45:08 -0400

    added neovim

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,21 @@
+return {
+	"catppuccin/nvim", 
+	name = "catppuccin", 
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			styles = {
+				comments = { "italic" },
+			},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+			},
+		})
+
+		vim.cmd.colorscheme "catppuccin"
+	end
+}
